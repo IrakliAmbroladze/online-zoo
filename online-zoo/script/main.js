e223a39a6b1df5626330c921f72e8ef40e13ac50
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-01 02:40:10 +0400

    feat(header): make navigation bar hoverable

--- online-zoo/script/main.js
+++ online-zoo/script/main.js
@@ -3,10 +3,11 @@ async function loadComponent(selector, url) {
   const res = await fetch(url);
   el.innerHTML = await res.text();
 
-  el.querySelector(".burger-menu").addEventListener("click", showMenu);
+  el.querySelector(".burger-menu").addEventListener("click", toggleMenu);
+  el.querySelector(".close-modal").addEventListener("click", toggleMenu);
 }
 
-function showMenu() {
+function toggleMenu() {
   const modal = document.querySelector(".burger-modal");
   if (modal) {
     modal.classList.toggle("show-burger-modal");
