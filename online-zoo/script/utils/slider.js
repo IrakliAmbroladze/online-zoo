bc21bd32aa7262834d8c27a239ae7bfc3ae61fb5
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-06 20:27:23 +0400

    feat: added transform translate style via JS

--- online-zoo/script/utils/slider.js
+++ online-zoo/script/utils/slider.js
@@ -1,12 +1,13 @@
 export const slider = () => {
   const left_arrow = document.getElementById("sldr_left_arr");
   const right_arrow = document.getElementById("sldr_right_arr");
+  const slider = document.getElementById("slider");
   left_arrow.addEventListener("click", () => {
     console.log("clicked on left arrow");
+    slider.style.transform = "translate(-50px)";
   });
   right_arrow.addEventListener("click", () => {
     console.log("clicked on right arrow");
+    slider.style.transform = "translate(50px)";
   });
-  const slider = document.getElementById("slider");
-  console.log("slider is: ", slider);
 };
