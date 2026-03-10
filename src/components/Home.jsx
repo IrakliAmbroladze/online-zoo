be8cea07f3fcd71cd21854a8ec3f405f061d3b33
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-11 00:20:03 +0400

    feat(Home): added html content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,224 @@
 const Home = () => {
-  return <div></div>;
+  return (
+    <>
+      <main class="main">
+        <div class="container">
+          <div class="title-container">
+            <h1>Watch your favorite animal online</h1>
+            <h2>
+              Explore the exciting and mysterious world of wild animals in a
+              natural setting without leaving your home.
+            </h2>
+            <button class="btn btn--orange">
+              view live cam <img src="./assets/icons/arrow.svg" alt="arrow" />
+            </button>
+          </div>
+        </div>
+        <div class="cover-image">
+          <img src="./assets/images/Background.jpg" alt="" />
+        </div>
+      </main>
+      <section class="welcome">
+        <div class="container">
+          <div class="welcome-card-1">
+            <div class="welcome-text-content">
+              <h2>Welcome to the Online Zoo!</h2>
+              <p>
+                On our website, using live webcams, fans of all ages can observe
+                various animals. Among them, are Giant pandas, eagles,
+                alligators, forest gorillas, African lions, and others. It is
+                the whole natural world in real-time in front of our cameras. We
+                hope you will enjoy watching closely and explore animals’
+                behavior and habitats! Note: animals are not always on view on
+                cameras, so please check back if you don't see anything.
+              </p>
+            </div>
+            <div class="welcome-image">
+              <img src="./assets/images/welcome.jpg" alt="welcome" />
+            </div>
+          </div>
+          <div class="welcome-card-2">
+            <div class="welcome-text-content">
+              <h2>How we work</h2>
+              <p>
+                Online Zoo is a nonprofit committed to inspiring awareness and
+                preservation of nature and wild animals in our zoo and
+                worldwide. To continue these efforts, we need your help. We're
+                so grateful to our supporters. All donations, large and small,
+                go a long way to the conservation efforts of our pets.
+              </p>
+            </div>
+            <div class="welcome-image">
+              <img src="./assets/images/how_it_works.jpg" alt="how_it_works" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="donation">
+        <div class="container">
+          <div class="description">
+            <h2>Your donation makes a difference!</h2>
+            <p>
+              The Online Zoo's animal webcams are some of the most famous on the
+              internet. Tune in to watch your favourite animals — live, 24/7!
+            </p>
+          </div>
+          <div class="quick-donate">
+            <h3>Quick Donate</h3>
+            <div class="amount">
+              <span class="donation-amount">$ Donation Amount</span>
+              <button class="btn btn--orange">
+                <img src="./assets/icons/arrow.svg" alt="arrow" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="meet-pets">
+        <div class="container" id="pets-container">
+          <div class="intro">
+            <h2>Meet some our pets</h2>
+            <p>
+              Do you have a special place in your heart for animals? Who are
+              your favorites? Perhaps you'd like to donate to special ones or
+              all our pets? We think it's important for you to choose how your
+              donation is used.
+            </p>
+          </div>
+          <div class="slider-arrows">
+            <div class="left" id="sldr_left_arr"></div>
+            <div class="right" id="sldr_right_arr"></div>
+          </div>
+          <div class="slider-pets-in-zoo" id="slider">
+            <div class="animals-card">
+              <label>Liz</label>
+              <div class="cover">
+                <img src="./assets/images/koala.png" alt="koala" />
+              </div>
+              <div class="title">Australian Koala</div>
+              <p>
+                The elevated walkways bring you to eye level with the koalas as
+                they perch  in their forest.
+              </p>
+              <button class="btn btn--pure-text-orange">
+                <span>view live cam</span>
+                <img src="./assets/icons/arrow.svg" alt="arrow" />
+              </button>
+            </div>
+            <div class="animals-card">
+              <label>Shake</label>
+              <div class="cover">
+                <img src="./assets/images/lion.png" alt="lion" />
+              </div>
+              <div class="title">African Lion</div>
+              <p>
+                Lions roam the savannas and grasslands of Africa, hunting and
+                raising cubs in the pride.
+              </p>
+              <button class="btn btn--pure-text-orange">
+                <span>view live cam</span>
+                <img src="./assets/icons/arrow.svg" alt="arrow" />
+              </button>
+            </div>
+            <div class="animals-card">
+              <label>Senja</label>
+              <div class="cover">
+                <img src="./assets/images/tiger.png" alt="tiger" />
+              </div>
+              <div class="title">Sumatran Tiger</div>
+              <p>Sumatran Tigers are the smallest of the five sub-species.</p>
+              <button class="btn btn--pure-text-orange">
+                <span>view live cam</span>
+                <img src="./assets/icons/arrow.svg" alt="arrow" />
+              </button>
+            </div>
+            <div class="animals-card">
+              <label>Sam & Lora</label>
+              <div class="cover">
+                <img src="./assets/images/eagles.png" alt="eagles" />
+              </div>
+              <div class="title">West End Bald Eagles</div>
+              <p>
+                Pair of eagle parents lay and protect eggs, feed their chicks
+                and teach them to hunt and fly.
+              </p>
+              <button class="btn btn--pure-text-orange">
+                <span>view live cam</span>
+                <img src="./assets/icons/arrow.svg" alt="arrow" />
+              </button>
+            </div>
+            <div class="animals-card">
+              <label>Andy</label>
+              <div class="cover">
+                <img src="./assets/images/lemur.png" alt="lemur" />
+              </div>
+              <div class="title">Australian Koala</div>
+              <p>
+                The elevated walkways bring you to eye level with the koalas as
+                they perch  in their forest.
+              </p>
+              <button class="btn btn--pure-text-orange">
+                <span>view live cam</span>
+                <img src="./assets/icons/arrow.svg" alt="arrow" />
+              </button>
+            </div>
+            <div class="animals-card">
+              <label>Glen</label>
+              <div class="cover">
+                <img src="./assets/images/gorilla.png" alt="gorilla" />
+              </div>
+              <div class="title">Gorilla in Congo</div>
+              <p>
+                Variety of snacks very important for the healthy life of
+                gorillas and his plenty of babies.
+              </p>
+              <button class="btn btn--pure-text-orange">
+                <span>view live cam</span>
+                <img src="./assets/icons/arrow.svg" alt="arrow" />
+              </button>
+            </div>
+            <div class="animals-card">
+              <label>Mike</label>
+              <div class="cover">
+                <img src="./assets/images/alligator.png" alt="alligator" />
+              </div>
+              <div class="title">Chinese Alligator</div>
+              <p>
+                From nose to tail, belly to back, hard scales protect this
+                petite alligator.
+              </p>
+              <button class="btn btn--pure-text-orange">
+                <span>view live cam</span>
+                <img src="./assets/icons/arrow.svg" alt="arrow" />
+              </button>
+            </div>
+            <div class="animals-card">
+              <label>Lucas</label>
+              <div class="cover">
+                <img src="./assets/images/panda.png" alt="panda" />
+              </div>
+              <div class="title">Giant Panda</div>
+              <p>
+                Native to central China, giant pandas have come to symbolize
+                vulnerable species.
+              </p>
+              <button class="btn btn--pure-text-orange">
+                <span>view live cam</span>
+                <img src="./assets/icons/arrow.svg" alt="arrow" />
+              </button>
+            </div>
+          </div>
+          <button class="btn btn--font-navy btn-favorite">
+            <span>choose your favorite</span>
+            <img src="./assets/icons/arrow.svg" alt="arrow" />
+          </button>
+        </div>
+      </section>
+      <section class="pay-and-feed">
+        <h2>Pay and feed</h2>
+      </section>
+    </>
+  );
 };
 
 export default Home;
