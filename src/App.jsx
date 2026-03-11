5cbed16bec6d59bd20ffead6a3134ee94031891c
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-11 23:39:10 +0400

    feat: added JS content with dom manipulation

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,38 @@ import Zoos from "./components/Zoos";
 import ContactUs from "./components/ContactUs";
 
 function App() {
+  async function loadComponent(selector, url) {
+    const el = document.querySelector(selector);
+    const res = await fetch(url);
+    if (el !== null) {
+      el.innerHTML = await res.text();
+      el.querySelector(".burger-menu")?.addEventListener("click", toggleMenu);
+      el.querySelector(".close-modal")?.addEventListener("click", toggleMenu);
+    }
+    setActiveNavLink();
+  }
+
+  function toggleMenu() {
+    const modal = document.querySelector("#header .burger-modal");
+    modal?.classList.toggle("show-burger-modal");
+  }
+  function setActiveNavLink() {
+    const normalize = (path) => path.replace(/\/$/, "") || "/";
+
+    const currentPath = normalize(window.location.pathname);
+
+    const links = document.querySelectorAll(".nav-link");
+
+    links.forEach((link) => {
+      const linkPath = normalize(new URL(link.href).pathname);
+
+      link.classList.toggle("highlight", linkPath === currentPath);
+    });
+  }
+
+  loadComponent("#header", "/components/header.html");
+  loadComponent("#footer", "/components/footer.html");
+
   return (
     <div className="flex flex-col h-dvh">
       <Header />
