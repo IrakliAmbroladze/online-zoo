5272ff0ea1cf4591cc9c4c49f18db9b57687ccb7
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-16 02:43:37 +0400

    feat(RegisterForm): keep user data in localstorage after successful registration

--- online-zoo/src/utils/createForm.ts
+++ online-zoo/src/utils/createForm.ts
@@ -38,6 +38,10 @@ export const createForm = ({
   btnSubmit.classList.add("btn");
   btnSubmit.classList.add("btn--orange");
   form.appendChild(btnSubmit);
+
+  const serverErr = document.createElement("p");
+  form.appendChild(serverErr);
+
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
     const body: Record<string, string> = {};
@@ -54,17 +58,37 @@ export const createForm = ({
         body: JSON.stringify(body),
       });
       if (!response.ok) {
-        const errorBody = await response.json();
-        console.log("error body:", errorBody);
-      } else {
-        console.log("success response");
+        let message = "not able to register";
+        try {
+          const data = await response.json();
+          if (data?.error) {
+            message = data.error;
+          }
+          console.log(data);
+        } catch (err) {
+          console.log(err);
+        }
+        serverErr.textContent = message;
+        return;
       }
+
+      const { data } = await response.json();
+      const { user, access_token } = data;
+
+      if (user) {
+        localStorage.setItem("user", JSON.stringify(user));
+      }
+      if (access_token) {
+        localStorage.setItem("access_token", access_token);
+      }
+      window.location.href = "/";
     } catch (err) {
       if (err instanceof Error) {
         console.error("Caught an Error object:", err.message);
       } else {
         console.error("Caught an unknown error type:", err);
       }
+      serverErr.textContent = "Network error. Please try again.";
     }
   });
 };
